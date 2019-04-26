084c23c1d4984a219d7688e15fd46763795b974b
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-04-26 11:24:21 -0400

    rebase adrien's work onto master

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # nyc-hack
+
+## Index builder
+
+The `merge.js` script can merge restaurant data from a CSV file and a JSON file, in order to upload to an Algolia index.
+
+### Usage
+
+1. Download and unzip [project-files.zip](https://github.com/algolia/solutions-hiring-assignment/blob/master/project-files.zip)
+2. Make sure Node.js is installed, and run `$ npm install`
+3. Run `node merge.js > merged.json`
+
+### Other restaurant data sources
+
+- http://opentable.herokuapp.com/ (example: https://opentable.herokuapp.com/api/restaurants?name=jerk)
+- https://data.world/data-society/discover-the-menu
+- http://curatingmenus.org/data_dictionary
+- http://menus.nypl.org/data
+- https://www.programmableweb.com/api/eatstreet
+- https://developers.eatstreet.com/
+- https://www.yelp.com/developers/documentation/v3/business_search
+- https://www.yelp.com/developers/documentation/v3/all_category_list
---
+++ merge.js
@@ -0,0 +1,54 @@
+console.error('loading restaurants_list.json...');
+const restaurantsSet = require('./restaurants_list.json').reduce((set, restaurant) => ({
+  ...set,
+  [restaurant.objectID]: restaurant
+}), {});
+
+// console.log(restaurantsSet['101422']);
+
+/*
+objectID: ///
+name: 'Town',
+address: '348 Main Street',
+area: 'Denver / Colorado',
+city: 'Carbondale',
+country: 'US',
+image_url: 'https://www.opentable.com/img/restimages/101422.jpg',
+mobile_reserve_url: 'http://mobile.opentable.com/opentable/?restId=101422',
+payment_options: [ 'AMEX', 'Discover', 'MasterCard', 'Visa' ],
+phone: '9709636328',
+postal_code: '81623',
+price: 2,
+reserve_url: 'http://www.opentable.com/single.aspx?rid=101422',
+state: 'CO',
+_geoloc: { lat: 39.400235, lng: -107.210373 } }
+*/
+
+const additionalFields = ['name', 'address', 'area', 'city', 'country', 'image_url', 'mobile_reserve_url', 'phone', 'postal_code', 'price', 'mobile_reserve_url', 'state', /*'_geoloc'*/]
+
+const getRestaurantFields = id => {
+  const restaurantData = restaurantsSet[id] || {};
+  const values = additionalFields.map(key => restaurantData[key])
+  //console.log(id, !!restaurantData, values);
+  return values;
+};
+
+const lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('./restaurants_info.csv')
+});
+
+let header = null;
+
+lineReader.on('line', function (line) {
+  if (!header) {
+    header = line.split(';').concat(additionalFields);
+    console.log(header.map(str => `"${str}"`).join(','));
+  } else {
+    const [ id ] = line.split(';');
+    console.log(line.split(';').concat(getRestaurantFields(id)).map(str => `"${str}"`).join(','));
+  }
+});
+
+lineReader.on('close', function (line) {
+  //console.log(first.split(';'));
+});
