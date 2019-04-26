16050514c447e59c35bbf1231316dc107051491c
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-04-26 11:24:58 -0400

    no message

--- merge.js
+++ merge.js
@@ -1,3 +1,30 @@
+const fs = require('fs');
+const csv2json = require('csv2json');
+
+const CSV_FILE = 'restaurants_info.csv';
+
+// parse a stringified JSON object, or return undefined if it's not valid JSON
+const parseJSON = (str) => {
+  try {
+    return JSON.parse(str);
+  } catch (err) {
+    return undefined;
+  }
+}
+ 
+// change types of some fields from the csv file
+const transformRestData = (rest) => ({
+  objectID: parseInt(rest.objectID, 10),
+  food_type: rest.food_type,
+  stars_count: parseFloat(rest.stars_count),
+  reviews_count: parseInt(rest.reviews_count, 10),
+  neighborhood: rest.neighborhood,
+  phone_number: rest.phone_number,
+  price_range: rest.price_range,
+  dining_style: rest.dining_style
+});
+
+// read a JSON data file and returns a objectID->object set
 function readExtData (file) {
   console.error(`loading ${file}...`);
   return require(file).reduce((set, restaurant) => ({
@@ -6,52 +33,33 @@ function readExtData (file) {
   }), {});
 }
 
-const restaurantsSet = readExtData('./restaurants_list.json');
-// console.log(restaurantsSet['101422']);
-
-/*
-objectID: ///
-name: 'Town',
-address: '348 Main Street',
-area: 'Denver / Colorado',
-city: 'Carbondale',
-country: 'US',
-image_url: 'https://www.opentable.com/img/restimages/101422.jpg',
-mobile_reserve_url: 'http://mobile.opentable.com/opentable/?restId=101422',
-payment_options: [ 'AMEX', 'Discover', 'MasterCard', 'Visa' ],
-phone: '9709636328',
-postal_code: '81623',
-price: 2,
-reserve_url: 'http://www.opentable.com/single.aspx?rid=101422',
-state: 'CO',
-_geoloc: { lat: 39.400235, lng: -107.210373 } }
-*/
-
-const additionalFields = ['name', 'address', 'area', 'city', 'country', 'image_url', 'mobile_reserve_url', 'phone', 'postal_code', 'price', 'mobile_reserve_url', 'state', /*'_geoloc'*/]
-
-const getRestaurantFields = id => {
-  const restaurantData = restaurantsSet[id] || {};
-  const values = additionalFields.map(key => restaurantData[key])
-  //console.log(id, !!restaurantData, values);
-  return values;
-};
-
-const lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('./restaurants_info.csv')
+const printEnrichedRecordsFromFile = ({ csvFile, restaurantsSet }) => new Promise((resolve, reject) => {
+  let first = true;
+  fs.createReadStream(csvFile)
+    .pipe(csv2json({
+      separator: ';' // (defaults to comma)
+    }))
+    .on('data', data => {
+      const json = parseJSON(data.toString());
+      if (typeof json !== 'object') return;
+      const rest = transformRestData(json);
+      const extData = restaurantsSet[rest.objectID];
+      console.log((first ? '[' : ',') + JSON.stringify({
+        ...rest,
+        ...extData
+      }));
+      first = false;
+    })
+    .on('end', () => {
+      console.log(']');
+      resolve();
+    })
+    .on('error', reject);
 });
 
-let header = null;
-
-lineReader.on('line', function (line) {
-  if (!header) {
-    header = line.split(';').concat(additionalFields);
-    console.log(header.map(str => `"${str}"`).join(','));
-  } else {
-    const [ id ] = line.split(';');
-    console.log(line.split(';').concat(getRestaurantFields(id)).map(str => `"${str}"`).join(','));
-  }
-});
-
-lineReader.on('close', function (line) {
-  //console.log(first.split(';'));
-});
+// actual script
+(async () => {
+  const restaurantsSet = readExtData('./restaurants_list.json');
+  // console.log(restaurantsSet['101422']);
+  await printEnrichedRecordsFromFile({ csvFile: CSV_FILE, restaurantsSet });
+})();
